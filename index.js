ec0b8857ba5c67268b1c307096219bab32c7811d
Ryan Durand <73862570+ryan-durand@users.noreply.github.com>
2021-01-21 11:27:08 -0500

    Uploading correct files
    Initial commit had the wrong files uploaded

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+var randomNumber1 = Math.floor(Math.random()*6+1); //1-6
+var randomNumber2 = Math.floor(Math.random()*6+1); // 1-6
+var randImg1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+var randImg2 = "images/dice" + randomNumber2 + ".png";
+document.getElementsByClassName("img1")[0].setAttribute("src", randImg1);
+document.getElementsByClassName("img2")[0].setAttribute("src", randImg2);
+if(randomNumber1 > randomNumber2){
+  document.querySelector("div h1").innerText = "🚩 Player 1 Wins!";
+}
+if(randomNumber2 > randomNumber1){
+  document.querySelector("div h1").innerText = "Player 2 Wins! 🚩";
+}
+if(randomNumber2 == randomNumber1){
+  document.querySelector("div h1").innerText = "Draw!";
+}
